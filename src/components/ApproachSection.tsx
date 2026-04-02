a07dab6a6607928c7e90f382df508156c34267c3
gafamoomin <shotaro.asano@icloud.com>
2026-04-02 18:32:05 +0900

    中央値と平均の表現修正

--- src/components/ApproachSection.tsx
+++ src/components/ApproachSection.tsx
@@ -40,7 +40,7 @@ export default function ApproachSection() {
                 </div>
                 <div className={styles.barLabels}>
                   <span className={styles.barRange}>{a.min}〜{a.max}名</span>
-                  <span className={styles.barMid}>中央値 {a.mid}名</span>
+                  <span className={styles.barMid}>推定値 {a.mid}名</span>
                 </div>
               </div>
             </div>
@@ -48,7 +48,7 @@ export default function ApproachSection() {
         </div>
 
         <div className={styles.avgBox}>
-          <div className={styles.avgLabel}>5アプローチの平均レンジ</div>
+          <div className={styles.avgLabel}>5アプローチの総合推定レンジ</div>
           <div className={styles.avgValue}>約30〜58名</div>
         </div>
       </div>
